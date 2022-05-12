0eecaa1f3031547281b182ba793ad228e32049ed
Tarcila Garcia <tarcilasg@gmail.com>
2022-05-12 11:27:57 -0300

    feat :sparkles: add: create button dashboard

--- distoq-capstonem3/src/Providers/cart/index.js
+++ distoq-capstonem3/src/Providers/cart/index.js
@@ -6,17 +6,17 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addCart = (product) => {
-    const list = JSON.parse(localStorage.getItem("@cart")) || [];
+    const list = JSON.parse(localStorage.getItem("@DEStoq:cart")) || [];
     list.push(product);
     setCart(list);
     console.log("list dentro do addCart", list);
-    localStorage.setItem("@cart", JSON.stringify(list));
+    localStorage.setItem("@DEStoq:cart", JSON.stringify(list));
   };
 
   const deleteCart = (id) => {
     const newCart = cart.filter((item) => item.id !== id);
     setCart(newCart);
-    localStorage.setItem("@cart", JSON.stringify(newCart));
+    localStorage.setItem("@DEStoq:cart", JSON.stringify(newCart));
   };
 
   return (
--- distoq-capstonem3/src/pages/home/index.jsx
+++ distoq-capstonem3/src/pages/home/index.jsx
@@ -1,18 +1,34 @@
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { ShowcaseContext } from "../../Providers/showcase";
+import { useJwt } from "react-jwt";
+
+import { ShowcaseContext } from "../../Providers/showcase/";
 import { CartContext } from "../../Providers/cart";
+
+import { GrUserAdmin } from "react-icons/gr";
 
 const Home = ({ product }) => {
   const navigate = useNavigate();
+  const tokenUser = JSON.parse(localStorage.getItem("@DEStoq:token")) || "";
+  const { decodedToken, isExpired } = useJwt(tokenUser);
   const { listProducts } = useContext(ShowcaseContext);
   const { cart, addCart } = useContext(CartContext);
+  const [isAdmin, setIsAdmin] = useState(false);
   console.log("cart na Home page", cart);
+
+  if (decodedToken?.sub === "1") {
+    return setIsAdmin(!isAdmin);
+  }
 
   return (
     <main>
       <h1>hamburgueria</h1>
-      <button onClick={() => navigate("/cart")}>ver carrinho</button>
+      <header>
+        <button onClick={() => navigate("/cart")}>ver carrinho</button>
+        <button disable={isAdmin} onClick={() => navigate("/dashboard")}>
+          <GrUserAdmin />
+        </button>
+      </header>
       <ul>
         {listProducts.map((product) => (
           <li key={product.id}>
